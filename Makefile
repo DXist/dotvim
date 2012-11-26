e1bd8231df452f0f8cc717ce800e0d2c47cc8e2e
Rinat Shigapov <rinatshigapov@gmail.com>
2012-11-26 14:51:05 +0400

    move to vundle

---
+++ Makefile
@@ -0,0 +1,5 @@
+default:
+	vim +BundleInstall +qall
+
+install:
+	git clone https://github.com/gmarik/vundle.git bundle/vundle
--- vim_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PROJECT=`python -c "import os; print os.path.basename(os.getcwd())"`
-export PYTHONPATH=`python -c "import os; print os.path.realpath(os.path.join(os.getcwd(),'..'))"`
-export DJANGO_SETTINGS_MODULE=$PROJECT.settings
-export DJANGO_APP=anapp
-vim $@
